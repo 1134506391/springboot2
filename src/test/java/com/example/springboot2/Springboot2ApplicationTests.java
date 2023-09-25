2d181b42bbfe23dd5aecbb3f74909c424af2bbe7
3hpyj <3hpyj@3h.net>
2023-09-25 18:04:09 +0800

    feat: 单元测试

--- src/test/java/com/example/springboot2/Springboot2ApplicationTests.java
+++ src/test/java/com/example/springboot2/Springboot2ApplicationTests.java
@@ -1,13 +1,31 @@
 package com.example.springboot2;
 
+import com.example.springboot2.domain.Student;
+import com.example.springboot2.domain.User;
+import com.example.springboot2.req.UserReq;
+import com.example.springboot2.service.StudentScoreService;
+import com.example.springboot2.service.UserService;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Slf4j
 class Springboot2ApplicationTests {
+
+    @Autowired
+    private StudentScoreService studentScoreService;
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    public void testInsertUser(){
+
+       log.info("queryList:"+studentScoreService.queryList());
+
     }
 
 }
